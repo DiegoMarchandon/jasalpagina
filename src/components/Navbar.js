395e47b5a1ae728880225affb7dfffe20ac84fbb
DiegoMarchandon <diegomarc2020@gmail.com>
2025-04-09 15:05:36 -0300

    navbar styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,11 +2,12 @@
 en lugar de las etiquetas <a> normales cuando queremos navegar 
 dentro de nuestra aplicación sin recargar la página */
 import { Link } from "react-router-dom";
+import './css/Navbar.css';
 
 function Navbar(){
     return (
-        <nav>
-            <ul>
+        <nav className="navbar">
+            <ul className="navbar-links">
                 <li><Link to="/">Inicio</Link></li>
                 <li><Link to="/sobre-mi">Sobre mí</Link></li>
                 <li><Link to="/servicios">Servicios</Link></li>
