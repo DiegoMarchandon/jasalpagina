0cb9d46171c4d596ce9a2a54b1c9379a9baf985b
DiegoMarchandon <diegomarc2020@gmail.com>
2025-04-09 13:25:43 -0300

    inicializo navbar + footer + header + routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,13 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Inicio from './pages/Inicio';
+import SobreMi from './pages/SobreMi';
+import Servicios from './pages/Servicios';
+import Portfolio from './pages/Portfolio';
+import Blog from './pages/Blog';
+import Contacto from './pages/Contacto';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
