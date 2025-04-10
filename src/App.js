4f7ebef25314d73cf4e1b09e414ecd45166b7412
DiegoMarchandon <diegomarc2020@gmail.com>
2025-04-10 10:05:55 -0300

    deleting unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// import logo from './logo.svg';
 import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Inicio from './pages/Inicio';
