c03c632b76a67029b861172ae7969d61f2934204
DiegoMarchandon <diegomarc2020@gmail.com>
2025-04-14 11:03:43 -0300

    dinamic <li> items + header Font

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
+import JasalFuente from "../assets/JasalFuente.png"; 
 import './css/Header.css';
 
 function Header(){
     return (
       <header className="header-artista">
-        <h1 className="titulo-principal">Jasal</h1>
+        {/* <h1 className="titulo-principal">Jasal</h1> */}
+        <img src={JasalFuente} alt="Logo de Jasal" className="logo-artista" />
         <p className="subtitulo">Escribo para quienes se enamoran rápido y se olvidan lento 🥀🪞</p>
       </header>
     )
--- src/pages/Inicio/Inicio.jsx
+++ src/pages/Inicio/Inicio.jsx
@@ -1,6 +1,26 @@
 import './styles.css';
 import Tachuela from '../../assets/thumb-tack.png';
+import React, {useState} from 'react';
 function Inicio() {
+
+  // lógica de mi función:
+  const [datos, setDatos] = useState([
+    {
+      fecha: "20 Mar 2025",
+      lugar: "Teatro Gran Rex, Buenos Aires",
+      enlace: "#enlace1"
+    },
+    {
+      fecha: "27 Mar 2025",
+      lugar: "Sala Zitarrosa, Montevideo",
+      enlace: "#enlace2"
+    },
+    {
+      fecha: "5 Abr 2025",
+      lugar: "Niceto Club, Buenos Aires",
+      enlace: "#enlace3"
+    }]);
+
   return (
     <div className="site-inicio">
       <div className="inicio-content">
@@ -9,51 +29,25 @@ function Inicio() {
         <a href="#contacto" className="inicio-btn">Contáctame</a>
       </div>
       <section id="eventos">
-          <h2>Próximos Eventos!</h2>
-          <img src={Tachuela} alt="tachuela logo" id="tachuela-img"/>
+        <h2>Próximos Eventos!</h2>
+        <img src={Tachuela} alt="tachuela logo" id="tachuela-img"/>
         <ul className="eventos-lista">
-          <li>
-            <div className='info-eventos'>
-            <span className="fecha">20 Abr 2025</span>
-            <span className="lugar">Teatro Ópera, Buenos Aires</span>
-            </div>
-            <div className='direccion-container'>
-              <a href="" className="enlaces-inicio">Comprar entradas</a>
-              <span className="flechas">
-                <span>&gt;</span>
-                <span>&gt;</span>
-                <span>&gt;</span>
-              </span>
-            </div>
-          </li>
-          <li>
-            <div className='info-eventos'>
-            <span className="fecha">27 Abr 2025</span>
-            <span className="lugar">Sala Zitarrosa, Montevideo</span>
-            </div>
-            <div className='direccion-container'>
-              <a href="" className="enlaces-inicio">Comprar entradas</a>
-              <span className="flechas">
-                <span>&gt;</span>
-                <span>&gt;</span>
-                <span>&gt;</span>
-              </span>
-            </div>
-          </li>
-          <li>
-            <div className='info-eventos'>
-            <span className="fecha">5 May 2025</span>
-            <span className="lugar">Niceto Club, Buenos Aires</span>
-            </div>
-            <div className='direccion-container'>
-              <a href="" className="enlaces-inicio">Comprar entradas</a>
-              <span className="flechas">
-                <span>&gt;</span>
-                <span>&gt;</span>
-                <span>&gt;</span>
-              </span>
-            </div>
-          </li>
+          {datos.map((data, index) => (
+            <li key={index}>
+              <div className='info-eventos'>
+                <span className="fecha">{data.fecha}</span>
+                <span className="lugar">{data.lugar}</span>
+              </div>
+              <div className='direccion-container'>
+                <a href={data.enlace} className="enlaces-inicio">Comprar entradas</a>
+                <span className="flechas">
+                  <span>&gt;</span>
+                  <span>&gt;</span>
+                  <span>&gt;</span>
+                </span>
+              </div>
+            </li>
+          ))}
         </ul>
       </section>
 
