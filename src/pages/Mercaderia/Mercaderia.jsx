51975af196c641ef3b09fc2f507beb09251e9d51
DiegoMarchandon <diegomarc2020@gmail.com>
2025-04-19 15:14:27 -0300

    mercaderia

--- src/pages/Mercaderia/Mercaderia.jsx
+++ src/pages/Mercaderia/Mercaderia.jsx
@@ -1,21 +1,30 @@
 import './Mercaderia.css';
 import {merch} from '../../services/services.js';
+import flecha from '../../../src/assets/right-arrow.png';
 
 function Mercaderia(){
     return (
-        <div className='merch-container'>
-            <h1 id='merch-main-title'>mi mercadería</h1>
-            {
-            // en JSX, cualquier lógica de renderizado dinámica (o código JS) debe ir entre {}
-            merch.map(item => (
-                <div key={item.id} className="merch-item">
-                    <img className='item-image' src={item.images[0]} alt={item.alt} />
-                    <h2 className='item-text item-title'>{item.title}</h2>
-                    <p className='item-text item-price'>Precio: ${item.price}</p>
-                    <p className='item-text item-description'>{item.description}</p>
-                </div>
-            ))
-            }
+  
+        <div>
+                <h1 id='merch-main-title'>mi mercadería</h1>
+            <div className='merch-container'>
+                {
+                // en JSX, cualquier lógica de renderizado dinámica (o código JS) debe ir entre {}
+                merch.map(item => (
+                    <div key={item.id} className="merch-item">
+                        <div className='images-container'>
+                  
+                            <button className='prev item-button' style={{transform:'rotate(180deg)'}}><img src={flecha} alt="flecha logo" /></button>
+                            <img className='item-image' src={item.images[0]} alt={item.alt} />
+                            <button className='next item-button'><img src={flecha} alt="flecha logo" /></button>
+                        </div>
+                        <h2 className='item-text item-title'>{item.title}</h2>
+                        <p className='item-text item-price'>Precio: ${item.price}</p>
+                        <p className='item-text item-description'>{item.description}</p>
+                    </div>
+                ))
+                }
+            </div>
         </div>
     // <h1>Bienvenido a la mercaderia de la artista</h1>
     );
