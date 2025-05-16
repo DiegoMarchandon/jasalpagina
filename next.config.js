1eb6e0e721d01005ab9f4dc4b997c8fbb96cb326
DiegoMarchandon <diegomarc2020@gmail.com>
2025-05-16 11:24:18 -0300

    assetPrefix vacio

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   // output: isProd ? 'export' : undefined,
-  assetPrefix: isProd ? '/jasalreact/' : '',
+  assetPrefix: /* isProd ? '/jasalreact/' : */ '',
   images: {
     unoptimized: true, // Para evitar optimizaciones que no funcionan en export estático
   },
